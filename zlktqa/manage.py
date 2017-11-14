3d7c01a87c5e0d7fd488a66c10a0088d1bba96d5
phd <837363308@qq.com>
2017-11-14 17:06:06 +0800

    User模型创建

--- zlktqa/manage.py
+++ zlktqa/manage.py
@@ -3,8 +3,9 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from zlktqa import app
 from exts import db
+from models import User
 
-manager = Mnager(app)
+manager = Manager(app)
 
 # 使用Migrate绑定app和db
 migrate = Migrate(app, db)
